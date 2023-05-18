b25badb5877e8f42f2f2689032242f9f8a481ea8
Michael Andrews <72107310+mandrewspicard@users.noreply.github.com>
2023-05-17 17:46:43 -0700

    Frontend Expanded

--- src/main/java/MGB/CloudwatchFX/FXController.java
+++ src/main/java/MGB/CloudwatchFX/FXController.java
@@ -27,22 +27,29 @@ public class FXController
 	@FXML
 	public RadioButton weatherSunny, weatherRain, weatherSnow;
 	@FXML
-	public Label hotWeatherOut, hotLocationOut, hotTempOut, day1, day2, day3, day4, day5, day6, day7, summary1, summary2, summary3, summary4, summary5, summary6, summary7;
+	public Label hotWeatherOut, hotLocationOut, hotTempOut, day1, day2, day3, day4, day5, day6, day7, summary1, summary2, summary3, summary4, summary5, summary6, summary7, alertBox;
+	@FXML
+	public Label time1, time2, time3, time4, time5, time6, time7, temp1, temp2, temp3, temp4, temp5, temp6, temp7, weather1, weather2, weather3, weather4, weather5, weather6, weather7;
+	
 	@FXML
 	public TextField hotTempIn, cityInput;
+	
+	
 	
 	
 	public FXController() throws Exception
 	{
 			backend = new Client();
 			cityCode = "san-diego";
-    		weatherData = backend.dailyQuery("cityCode");
+			weatherData = backend.dailyQuery("cityCode");
 	}
 
 
 	
 	public void swapToDaily(ActionEvent event) throws Exception
 	{
+
+		weatherData = backend.dailyQuery("cityCode");
 		FXMLLoader loader = new FXMLLoader(getClass().getResource("Daily.fxml"));
 		Parent root = loader.load();
 		Scene scene = new Scene(root);
@@ -75,28 +82,73 @@ public class FXController
 		stage.show();
 	}
 	
-	public void swapToHourly(ActionEvent event) throws IOException
+	public void swapToHourly(ActionEvent event) throws Exception
 	{
-		root = FXMLLoader.load(getClass().getResource("Hourly.fxml"));
+		weatherData = backend.hourlyQuery("cityCode");
+		FXMLLoader loader = new FXMLLoader(getClass().getResource("Hourly.fxml"));
+		Parent root = loader.load();
+		Scene scene = new Scene(root);
 		stage = (Stage)((Node)event.getSource()).getScene().getWindow();
-		scene = new Scene(root);
 		stage.setScene(scene);
+		
+		   		
+		FXController controller = loader.getController();
+		
+		controller.time1.setText(weatherData.getJSONObject("hourly").getJSONArray("data").getJSONObject(0).getString("date").substring(11,16));
+		controller.time2.setText(weatherData.getJSONObject("hourly").getJSONArray("data").getJSONObject(1).getString("date").substring(11,16));
+		controller.time3.setText(weatherData.getJSONObject("hourly").getJSONArray("data").getJSONObject(2).getString("date").substring(11,16));
+		controller.time4.setText(weatherData.getJSONObject("hourly").getJSONArray("data").getJSONObject(3).getString("date").substring(11,16));
+		controller.time5.setText(weatherData.getJSONObject("hourly").getJSONArray("data").getJSONObject(4).getString("date").substring(11,16));
+		controller.time6.setText(weatherData.getJSONObject("hourly").getJSONArray("data").getJSONObject(5).getString("date").substring(11,16));
+		controller.time7.setText(weatherData.getJSONObject("hourly").getJSONArray("data").getJSONObject(6).getString("date").substring(11,16));
+		
+		controller.temp1.setText(String.valueOf((int)weatherData.getJSONObject("hourly").getJSONArray("data").getJSONObject(0).getDouble("temperature")) + "°F");
+		controller.temp2.setText(String.valueOf((int)weatherData.getJSONObject("hourly").getJSONArray("data").getJSONObject(1).getDouble("temperature")) + "°F");
+		controller.temp3.setText(String.valueOf((int)weatherData.getJSONObject("hourly").getJSONArray("data").getJSONObject(2).getDouble("temperature")) + "°F");
+		controller.temp4.setText(String.valueOf((int)weatherData.getJSONObject("hourly").getJSONArray("data").getJSONObject(3).getDouble("temperature")) + "°F");
+		controller.temp5.setText(String.valueOf((int)weatherData.getJSONObject("hourly").getJSONArray("data").getJSONObject(4).getDouble("temperature")) + "°F");
+		controller.temp6.setText(String.valueOf((int)weatherData.getJSONObject("hourly").getJSONArray("data").getJSONObject(5).getDouble("temperature")) + "°F");
+		controller.temp7.setText(String.valueOf((int)weatherData.getJSONObject("hourly").getJSONArray("data").getJSONObject(6).getDouble("temperature")) + "°F");
+		
+		controller.weather1.setText(weatherData.getJSONObject("hourly").getJSONArray("data").getJSONObject(0).getString("summary"));
+		controller.weather2.setText(weatherData.getJSONObject("hourly").getJSONArray("data").getJSONObject(1).getString("summary"));
+		controller.weather3.setText(weatherData.getJSONObject("hourly").getJSONArray("data").getJSONObject(2).getString("summary"));
+		controller.weather4.setText(weatherData.getJSONObject("hourly").getJSONArray("data").getJSONObject(3).getString("summary"));
+		controller.weather5.setText(weatherData.getJSONObject("hourly").getJSONArray("data").getJSONObject(4).getString("summary"));
+		controller.weather6.setText(weatherData.getJSONObject("hourly").getJSONArray("data").getJSONObject(5).getString("summary"));
+		controller.weather7.setText(weatherData.getJSONObject("hourly").getJSONArray("data").getJSONObject(6).getString("summary"));
+		
 		
 
 		
 		stage.show();
 	}
 	
-	public void swapToAlerts(ActionEvent event) throws IOException
+	public void swapToAlerts(ActionEvent event) throws Exception
 	{
-		root = FXMLLoader.load(getClass().getResource("Alerts.fxml"));
+		weatherData = backend.alertsQuery("cityCode");
+		FXMLLoader loader = new FXMLLoader(getClass().getResource("Alerts.fxml"));
+		Parent root = loader.load();
+		Scene scene = new Scene(root);
 		stage = (Stage)((Node)event.getSource()).getScene().getWindow();
-		scene = new Scene(root);
-		stage.setScene(scene);;
+		stage.setScene(scene); 		
+		FXController controller = loader.getController();
+		
+		if (weatherData.getJSONObject("alerts").getJSONArray("data").length() != 0)
+		{
+			String Alerts = "";
+			for (int i = 0; i < weatherData.getJSONObject("alerts").getJSONArray("data").length(); i++)
+			{
+			Alerts += weatherData.getJSONObject("alerts").getJSONArray("data").getJSONObject(i).getString("headline") + "\n";
+			}
+			controller.alertBox.setText(Alerts);
+		}
+		
+		
 		stage.show();
 	}
 	
-	public void swapToHot(ActionEvent event) throws IOException
+	public void swapToHot(ActionEvent event) throws Exception
 	{
 		root = FXMLLoader.load(getClass().getResource("HotLocation.fxml"));
 		stage = (Stage)((Node)event.getSource()).getScene().getWindow();
@@ -105,7 +157,7 @@ public class FXController
 		stage.show();
 	}
 	
-	public void hotFindIt(ActionEvent event) throws IOException
+	public void hotFindIt(ActionEvent event) throws Exception
 	{
 		if (weatherSunny.isSelected())
 		{
